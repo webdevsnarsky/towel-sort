f2eaa4256228099c5365d004c969f9748d041821
Alexander <snarsky93@gmail.com>
2020-02-21 17:07:00 +0300

    feat: add decision of task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,16 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if (matrix == null) {return []};
+  let arr = [];
+  let done = matrix.forEach((elem, i) => {
+    if (i%2 === 0) {
+       elem.forEach(item => arr.push(item));
+    } else {
+        let das = elem.reverse();
+        das.forEach(item => arr.push(item));
+    }
+
+    });
+  return arr;
 }
